0ffc375bdebcc5eb779ec8d0c26e14e9e394a46f
flyjgh <doctorzwei@gmail.com>
2022-02-13 23:39:28 +0100

    added getindex + setindex! and specialized types

--- src/core.jl
+++ src/core.jl
@@ -11,7 +11,7 @@ isvar(x::T) where T = x isa Symbol
 
 # ----------------------------------------------------------
 # Cartesian Product
-struct ×{α,β}
+mutable struct ×{α,β}
     l::α
     r::β
 end
@@ -24,25 +24,30 @@ unit(α) = α × ()
 car(x::×) = x.l
 cdr(x::×) = x.r
 
-Base.length(ρ::×{α,×}) where α = 1 + Base.length(ρ.r)
+Base.length(ρ::×{α,×{β,γ}}) where {α,β,γ} = 1 + Base.length(ρ.r)
 Base.length(ρ::×) = 2
 
 Base.firstindex(ρ::×) = 1
-Base.lastindex(ρ::×{α,×}) where α = length(ρ)
+Base.lastindex(ρ::×{α,×{β,γ}}) where {α,β,γ} = length(ρ)
 Base.lastindex(ρ::×{α,Tuple{}}) where α = 1
 Base.lastindex(ρ::×) = 2
 
+Base.getindex(ρ::×, i::α) where α <: Integer = i <= 1 ? ρ.l : ρ.r[i-1]
+Base.getindex(ρ::×, r::α) where α <: AbstractRange = (r .|> σ -> ρ[σ])
+Base.setindex!(ρ::×, val, i::α) where α <: Integer = i <= 1 ? ρ.l = val : setindex!(ρ.r, val, i-1)
+# Base.setindex!(ρ::×, val, r::α) where α <: AbstractRange = (ρ[σ] = val for σ ∈ r)
+
 Base.iterate(ρ::×, θ=1) = θ > length(ρ) ? nothing : (ρ[θ], θ+1)
-
-Base.getindex(ρ::×, i::α) where α <: Integer  = i <= 1 ? ρ.l : ρ.r[i .- 1]
-Base.setindex!(ρ::×, val, i::α) where α <: Integer  = ρ.data[i...] = val
-
 Base.BroadcastStyle(::Type{×}) = Broadcast.ArrayStyle{×}()
+
+Base.show(io::IO, ρ::×) = print(io, "(", .*(string.(x[begin:end-1]), " ")..., x[end], ")")
 
 # x = ×(2:5...)
 # x = 1 × x
 # length(x)
-# x[1]
+# x[1] = 5
+# x[1:3] = [6,7,8]
+# x
 # print.(x)
 
 # ----------------------------------------------------------
@@ -56,7 +61,7 @@ cdr(x::Pair) = x.second
 cdr(x::Tuple) = tail(x)
 
 # ----------------------------------------------------------
-
+# miniKanren
 assp(_, ::Tuple{}) = ()
 function assp(ƒ, ρ)
     ƒ(car(car(ρ))) && return car(ρ)
@@ -82,7 +87,9 @@ function occurs(x, y, z)
     false
 end
 
+
 ext_s(x, v, s) = occurs(x,v,s) ? s : (x => v) => s
+
 
 # occurs(1, (1 => 0), (0 => 1))
 # occurs(0, (2 => 4), (1 => 3))
